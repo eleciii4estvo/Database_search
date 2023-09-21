358611571d26b1c0fbbef21e5c3e0eb12842ddcb
eleciii4estvo <gulevsky_iv@mail.ru>
2023-09-21 14:59:53 +0300

    ver 0.1

---
+++ app.js
@@ -0,0 +1,46 @@
+const list= document.querySelector('#list')
+const filter = document.querySelector('#filter')
+let USERS = []
+
+filter.addEventListener('input', (event)=>{  //Обрабатывает каждый введенный символ
+    // console.log(event.target.value)   //Выводит написанный текст после каждого введенного символа  
+    const value = event.target.value.toLowerCase()    
+    const filtredUsers = USERS.filter((user)=>{
+        return user.name.toLowerCase().includes(value)
+    })
+    render(filtredUsers)
+})
+
+async function start(){
+    list.innerHTML='Loading...'
+    try {
+        
+        const resp = await fetch('https://jsonplaceholder.typicode.com/users')     //Get запрос на сервер к json файлу. Возвращает promise
+        const data = await resp.json()
+        setTimeout(()=>{
+            USERS=data
+            render(data)
+        },2000)
+        
+    } catch (error) {
+        list.innerHTML='Something went wrong... Try again later'
+    }
+}
+
+function render(users=[]){
+    if (users.length!=0){
+        const html=users.map(toHTML).join('')
+        list.innerHTML=html
+    } else{
+        list.innerHTML=`No users found`
+    }
+
+}
+
+function toHTML(user){
+    return`
+        <li class="list_item">${user.name}</li>
+    `
+}
+
+start()
